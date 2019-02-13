485445eb8a793ffd87c0d021c21869f2e9ab7f5e
craizetip <44974305+craizetip@users.noreply.github.com>
2019-02-13 12:35:53 +0300

    JavaServlet

---
+++ ruivmit/src/main/java/ru/ran/servlets/Box.java
@@ -0,0 +1,5 @@
+package ru.ran.servlets;
+
+public interface Box {
+    double getWeight();
+}
---
+++ ruivmit/src/main/java/ru/ran/servlets/CostCalculator.java
@@ -0,0 +1,21 @@
+package ru.ran.servlets;
+
+import ru.ran.servlets.Box;
+
+public class CostCalculator {
+    private double kgCost;
+    private double kmCost;
+
+    public CostCalculator(double kgCost, double kmCost) {
+        this.kgCost = kgCost;
+        this.kmCost = kmCost;
+    }
+
+    public double calcCost(Box box, int km) {
+        return box.getWeight() * kgCost + km * kmCost;
+    }
+
+    public double calcCost(double kg, int km) {
+        return kg * kgCost + km * kmCost;
+    }
+}
